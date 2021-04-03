5fa67e386863997815cf65e66589b6b2ca942c7c
Anton Orekhovskiy <anton.o.0619@gmail.com>
2021-04-04 01:08:10 +0600

    pre deploy

--- service2/soa.lab3.service2/common/src/main/java/ejb/EbayStatelessBeanRemote.java
+++ service2/soa.lab3.service2/common/src/main/java/ejb/EbayStatelessBeanRemote.java
@@ -1,12 +1,10 @@
 package ejb;
 
-import models.ProductsList;
-
 import javax.ejb.Remote;
 import javax.ws.rs.core.Response;
 
 @Remote
 public interface EbayStatelessBeanRemote {
-    public String sayHello();
-    public Response filterProducts(String key, String value);
+    String sayHello();
+    Response filterProducts(String key, String value);
 }
--- service2/soa.lab3.service2/soa.lab3.service2.jaxrs/src/main/java/FilterResource.java
+++ service2/soa.lab3.service2/soa.lab3.service2.jaxrs/src/main/java/FilterResource.java
@@ -1,23 +1,45 @@
+import ejb.EbayStatelessBeanRemote;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.Properties;
 
 @Path("/filter")
 public class FilterResource {
+    private static final String SERVICE2_EJB_JNDI = "java:global/SOA3/EbayStatelessBean!ejb.EbayStatelessBeanRemote";
+
+    private static EbayStatelessBeanRemote getStatelessBean() throws NamingException {
+        Properties environment = new Properties();
+        InitialContext ejbRemoteContext = new InitialContext(environment);
+        String ejb_jndi = System.getenv("SOA_EJB_JNDI");
+        if (ejb_jndi == null || ejb_jndi.isEmpty()) ejb_jndi = SERVICE2_EJB_JNDI;
+        return (EbayStatelessBeanRemote) ejbRemoteContext.lookup(ejb_jndi);
+    }
+
+    @GET
+    @Path("/hello")
+    public Response getHello() throws NamingException {
+        return Response.status(Response.Status.OK).entity(getStatelessBean().sayHello()).build();
+    }
 
     @GET
     @Path("/manufacture-cost/{manufacture-cost}")
     @Produces("application/xml")
-    public Response getFilteredByManufactureCost(@PathParam("manufacture-cost") String cost) {
-        return null;
+    public Response getFilteredByManufactureCost(@PathParam("manufacture-cost") String cost)
+            throws NamingException {
+        return getStatelessBean().filterProducts("manufacture-cost", cost);
     }
 
     @GET
     @Path("/unit-of-measure/{unit-of-measure}")
     @Produces("application/xml")
-    public Response getFilteredByUnitOfMeasure(@PathParam("unit-of-measure") String unitOfMeasure) {
-        return null;
+    public Response getFilteredByUnitOfMeasure(@PathParam("unit-of-measure") String unitOfMeasure)
+            throws NamingException {
+        return getStatelessBean().filterProducts("manufacture-cost", unitOfMeasure);
     }
 }
